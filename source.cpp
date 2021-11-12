a2a64e16bc76e1780ad39ee63bc47a9333adca62
Knightmare88 <87238643+Knightmare88@users.noreply.github.com>
2021-11-11 19:21:37 -0800

    Fixed final draft ':3

--- source.cpp
+++ source.cpp
@@ -1,13 +1,13 @@
 /*
  * Class: CMSC140 CRN
  * Instructor:
- * Project<number>
- * Description: (Give a brief description for Project)
+ * Project 4
+ * Description: Employee absence summary
  * Due Date:
  * I pledge that I have completed the programming assignment independently.
    I have not copied the code from a student or any source.
    I have not given my code to any student.
-   Print your Name here: __________
+   Print your Name here: Knight
 */
 
 #include <iostream>
